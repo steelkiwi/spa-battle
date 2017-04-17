f63b8b014a800a85b1dd0219728aa6972fee79a6
alex <changli@steelkiwi.com>
2017-04-17 09:32:00 +0300

    Hash location strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,8 @@ const rootRoutes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(rootRoutes)
+    // Use hash location strategy because app will be deployed to GitHub Pages
+    RouterModule.forRoot(rootRoutes, { useHash: true })
   ],
   exports: [
     RouterModule
