94fdb7ecea15a3aad4eb80e6f0172a0513d37d99
sergeyusatyuk <usatiuk@steelkiwi.com>
2017-04-25 16:04:43 +0300

    removed html5 routing

--- src/configs/routes.js
+++ src/configs/routes.js
@@ -3,7 +3,7 @@ import $locationProvider from 'angular';
 import app from 'index';
 
 const config = ($urlRouterProvider, $locationProvider) => {
-    $locationProvider.html5Mode(true).hashPrefix('!');
+    $locationProvider.html5Mode(false).hashPrefix('');
     $urlRouterProvider.otherwise('/');
 };
 
