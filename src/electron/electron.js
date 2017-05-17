c36f61b08c6bd90f7891d9f3209bbf8d14df4752
alex <changli@steelkiwi.com>
2017-05-17 13:23:48 +0300

    Step 4 - mprove Electron Configuration

--- src/electron/electron.js
+++ src/electron/electron.js
@@ -1,6 +1,6 @@
 // src/electron/electron.js
 
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, session } = require('electron');
 
 let win;
 
@@ -8,7 +8,13 @@ function createWindow() {
   win = new BrowserWindow();
   win.maximize();
 
-  win.loadURL(`http://localhost:4200`);
+  win.loadURL(`file://${__dirname}/index.html`);
+  session.defaultSession.webRequest.onBeforeSendHeaders(
+    { urls: ['https://*.gateway.marvel.com/*'] },
+    (details, callback) => {
+      details.requestHeaders['Referer'] = 'https://steelkiwi.github.io';
+      callback({ cancel: false, requestHeaders: details.requestHeaders });
+    });
 
   win.on('closed', () => {
     win = null;
